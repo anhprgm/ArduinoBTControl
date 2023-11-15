3ad77101d48d4ba22a705b89edcdd80c9c159a3a
TEH <akaidosuki@gmail.com>
2023-11-15 11:36:09 +0700

    fix ANR

--- app/src/main/java/com/arduno/remotebt/ConfigureLed.java
+++ app/src/main/java/com/arduno/remotebt/ConfigureLed.java
@@ -2,14 +2,19 @@ package com.arduno.remotebt;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import com.arduno.remotebt.databinding.ActivityConfigureLedBinding;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 public class ConfigureLed extends AppCompatActivity {
     private static final String TAG = "HUUDIEN";
+    boolean isReceive = true;
 
     private MyViewModel viewModel;
     private ActivityConfigureLedBinding binding;
@@ -25,28 +30,46 @@ public class ConfigureLed extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(MyViewModel.class);
 
         binding.bat.setOnClickListener(v -> {
-            connectedThread.write("1");
+            connectedThread.send("1");
         });
-        binding.tat.setOnClickListener(v -> connectedThread.write("0"));
+        binding.tat.setOnClickListener(v -> connectedThread.send("0"));
 
-        binding.textView.setOnClickListener(v -> {
-            connectedThread.run(message -> {
-                viewModel.message.postValue(message);
-            });
-        });
-        viewModel.message.observe(this, message -> {
-            binding.textView.setText(message);
-            Handler handler = new Handler();
-            handler.postDelayed(() -> {
-                connectedThread.run(message1 -> {
-                    viewModel.message.postValue(message1);
-                });
-            }, 100);
-
-        });
         binding.stop.setOnClickListener(v -> {
-
+            if (isReceive) {
+                binding.stop.setText("start");
+                isReceive = false;
+            } else {
+                binding.stop.setText("stop");
+                isReceive = true;
+                receiverMessage();
+            }
         });
+
+        receiverMessage();
+        viewModel.message.observe(this, s -> {
+            binding.textView.setText(s);
+            receiverMessage();
+        });
+
+
     }
 
+    void receiverMessage() {
+        if (!isReceive) {
+            return;
+        }
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            ExecutorService service = Executors.newSingleThreadExecutor();
+
+            service.submit(() -> {
+                connectedThread.run(s -> {
+                    Log.d("TAG", "TAH");
+                    viewModel.message.postValue(s);
+                    service.shutdown();
+                });
+            });
+        }, 100);
+
+    }
 }
--- app/src/main/java/com/arduno/remotebt/MainActivity.java
+++ app/src/main/java/com/arduno/remotebt/MainActivity.java
@@ -1,6 +1,7 @@
 package com.arduno.remotebt;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -12,15 +13,14 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.arduno.remotebt.databinding.ActivityMainBinding;
 
 import java.util.Set;
 import java.util.UUID;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -29,36 +29,28 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    // Global variables we will use in the
+
+    private ActivityMainBinding binding;
     private static final String TAG = "HUUDIEN";
     private static final int REQUEST_ENABLE_BT = 1;
-    //We will use a Handler to get the BT Connection statys
     public static Handler handler;
-    private final static int ERROR_READ = 0; // used in bluetooth handler to identify message update
+    private final static int ERROR_READ = 0;
     BluetoothDevice arduinoBTModule = null;
-    UUID arduinoUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //We declare a default UUID to create the global variable
+    UUID arduinoUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     ConnectedThread connectedThread;
+    @SuppressLint("CheckResult")
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
-        //Intances of the Android UI elements that will will use during the execution of the APP
-        TextView btDevices = findViewById(R.id.btDevices);
-
-        Button connectToDevice = (Button) findViewById(R.id.connectToDevice);
-        Button seachDevices = (Button) findViewById(R.id.seachDevices);
-        Button nextActivity = findViewById(R.id.nextActivity);
-        Log.d(TAG, "Begin Execution");
-
-        //Using a handler to update the interface in case of an error connecting to the BT device
-        //My idea is to show handler vs RxAndroid
         handler = new Handler(Looper.getMainLooper()) {
             @Override
-            public void handleMessage(Message msg) {
+            public void handleMessage(@NonNull Message msg) {
                 switch (msg.what) {
 
                     case ERROR_READ:
@@ -72,11 +64,9 @@ public class MainActivity extends AppCompatActivity {
         };
 
         final Observable<ConnectedClass> connectToBTObservable = Observable.create(emitter -> {
-            Log.d(TAG, "Calling connectThread class");
             ConnectThread connectThread = new ConnectThread(arduinoBTModule, arduinoUUID, handler);
             connectThread.run();
             if (connectThread.getMmSocket().isConnected()) {
-                Log.d(TAG, "Calling ConnectedThread class");
                  connectedThread = new ConnectedThread(connectThread.getMmSocket());
                 if(connectedThread.getMmInStream() != null && connectedThread!= null)
                 {
@@ -86,15 +76,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             emitter.onComplete();
-
         });
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////// Find all Linked devices ///////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
         //Display all the linked BT Devices
-        seachDevices.setOnClickListener(view -> {
+        binding.seachDevices.setOnClickListener(view -> {
             //Check if the phone supports BT
             if (bluetoothAdapter == null) {
                 // Device doesn't support Bluetooth
@@ -133,41 +118,32 @@ public class MainActivity extends AppCompatActivity {
                             Log.d(TAG, "HC-05 found");
                             arduinoUUID = device.getUuids()[0].getUuid();
                             arduinoBTModule = device;
-                            connectToDevice.setEnabled(true);
+                            binding.connectToDevice.setEnabled(true);
                         }
-                        btDevices.setText(btDevicesString);
+                        binding.btDevices.setText(btDevicesString);
                     }
                 }
             }
             Log.d(TAG, "Button Pressed");
         });
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////// Call the observable to connect to the HC-05 ////////////////////////////////////////////
-        ////////////////////////////////////////////// If it connects, the button to configure the LED will be enabled  ///////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        binding.connectToDevice.setOnClickListener(view -> {
+            if (arduinoBTModule != null) {
+                connectToBTObservable.
+                        observeOn(AndroidSchedulers.mainThread()).
+                        subscribeOn(Schedulers.io()).
+                        subscribe(connectedToBTDevice -> {
+                            //valueRead returned by the onNext() from the Observable
+                            if(connectedToBTDevice.isConnected()){
+                               binding.nextActivity.setEnabled(true);
+                            }
+                            //We just scratched the surface with RxAndroid
+                        });
 
-        connectToDevice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (arduinoBTModule != null) {
-                    connectToBTObservable.
-                            observeOn(AndroidSchedulers.mainThread()).
-                            subscribeOn(Schedulers.io()).
-                            subscribe(connectedToBTDevice -> {
-                                //valueRead returned by the onNext() from the Observable
-                                if(connectedToBTDevice.isConnected()){
-                                    nextActivity.setEnabled(true);
-                                }
-                                //We just scratched the surface with RxAndroid
-                            });
-
-                }
             }
         });
 
-        //Next activity to configure the LED
-        nextActivity.setOnClickListener(v -> {
+        binding.nextActivity.setOnClickListener(v -> {
             MyApplication.getApplication().setupConnectedThread(connectedThread);
             Intent intent = new Intent(MainActivity.this, ConfigureLed.class);
             startActivity(intent);
